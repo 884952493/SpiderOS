796cfce6e5ed93e10b6cb591b436bb166207c101
hutaotao <hutaotao@xiaomi.com>
2025-03-28 11:32:19 +0800

    achieve print
    Signed-off-by: hutaotao <hutaotao@xiaomi.com>

---
+++ kernel/main.c
@@ -0,0 +1,14 @@
+#include "print.h"
+int main()
+{	
+   
+   put_str("kernel!\nim com1\bing!\n");
+   put_str("gg\n");
+   put_int(0x11111111);
+   put_char(' ');
+   put_int(32);
+   put_char(' ');
+   put_int(999);
+   while(1);
+   return 0;
+}
