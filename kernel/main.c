f5cb0c07e486cc7ecdf5de4ba80f7354f8ecf537
hutaotao <hutaotao@xiaomi.com>
2025-03-28 15:35:43 +0800

    Implement interupt

--- kernel/main.c
+++ kernel/main.c
@@ -1,17 +1,9 @@
 #include "print.h"
-int main()
-{	
-   put_str("********Welcome to SpiderOS********\n");
-   put_str("********  author:hutaotao  ********\n");
-   put_str("kernel!\nim com1\bing!\n");
-   put_str("string:");
-   put_str("gg\n");
-   put_str("\nnumber:");
-   put_int(0x11111111);
-   put_char(' ');
-   put_int(32);
-   put_char(' ');
-   put_int(999);
+#include "init.h"
+
+int main(void) {
+   put_str("I am kernel\n");
+   init_all();
+   asm volatile("sti");
    while(1);
-   return 0;
 }
