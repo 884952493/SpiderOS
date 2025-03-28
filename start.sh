90a52f9a77a2705fa0abe96eecaf1ad1c6832ddd
hutaotao <hutaotao@xiaomi.com>
2025-03-28 14:09:34 +0800

    add author flag

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+dd if=/dev/zero of=hd60M.img bs=1M count=60
+nasm -I include/ -o boot/mbr.bin boot/mbr.S
+dd if=boot/mbr.bin of=hd60M.img conv=notrunc
+nasm -I include/ -o boot/loader.bin boot/loader.S
+dd if=boot/loader.bin of=hd60M.img bs=512 count=3 seek=2 conv=notrunc
 nasm -f elf -o lib/kernel/print.o lib/kernel/print.S
 gcc -m32 -I lib/kernel/ -I lib/ -c -o kernel/main.o kernel/main.c
 ld -m elf_i386 -Ttext 0xc0001500 -e main -o kernel/kernel.bin \
