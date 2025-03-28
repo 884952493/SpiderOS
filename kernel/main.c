118893a18d9c439c2d508b3bfc49ee13726c10bc
hutaotao <hutaotao@xiaomi.com>
2025-03-28 14:14:06 +0800

    debug print

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,12 @@
 #include "print.h"
 int main()
 {	
-   
+   put_str("********Welcome to SpiderOS********\n");
+   put_str("********  author:hutaotao  ********\n");
    put_str("kernel!\nim com1\bing!\n");
+   put_str("string:");
    put_str("gg\n");
+   put_str("\nnumber:");
    put_int(0x11111111);
    put_char(' ');
    put_int(32);
